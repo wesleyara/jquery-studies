ac590241da958edebcbe86ad5ddb66a00b91d167
wesleyara <wes1gom@gmail.com>
2024-09-10 16:49:18 -0300

    feat: add manipulando_eventos

---
+++ src/001_manipulando_eventos/app.js
@@ -0,0 +1,8 @@
+$("#sum").click(() => {
+  const value1 = $("#value1").val();
+  const value2 = $("#value2").val();
+
+  const sum = parseInt(value1) + parseInt(value2);
+
+  $("#result").text(sum);
+});
